44757278a1b712da19fdb9b2252279365c76c884
MissVeronica <veronica@tranza.se>
2022-11-28 01:12:17 +0100

    Update um-white-list-email-domains.php

--- um-white-list-email-domains.php
+++ um-white-list-email-domains.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - White Listing Email Domains
  * Description:     Extension to Ultimate Member for white listing email domains. Settings at UM Settings -> Access -> Other
- * Version:         2.0.0
+ * Version:         2.1.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -34,14 +34,16 @@ function white_listed_email_domains( $args ) {
 
     if( isset( $args['user_email'] ) && is_email( $args['user_email'] )) {
         $email_domain = explode( '@', $args['user_email'] );        
-        if( !in_array( strtolower( $email_domain[1] ), $valid_email_domains )) {
+        if( !in_array( strtolower( $email_domain[1] ), $valid_email_domains ) &&
+            !in_array( strtolower( $args['user_email'] ), $valid_email_domains )) {
             exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
         }
     }
 
     if( isset( $args['username'] ) && is_email( $args['username'] )) {
         $email_domain = explode( '@', $args['username'] );        
-        if( !in_array( strtolower( $email_domain[1] ), $valid_email_domains )) {
+        if( !in_array( strtolower( $email_domain[1] ), $valid_email_domains ) &&
+            !in_array( strtolower( $args['username'] ), $valid_email_domains )) {
             exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
         }
     }
