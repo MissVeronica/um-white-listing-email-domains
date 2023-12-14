916108938e43ef08b299a046ac8d33f699e776cf
MissVeronica <veronica@tranza.se>
2023-12-14 13:26:39 +0100

    Update um-white-list-email-domains.php

--- um-white-list-email-domains.php
+++ um-white-list-email-domains.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - White Listing Email Domains/Addresses
  * Description:     Extension to Ultimate Member for white listing email domains and email addresses. Settings at UM Settings -> Access -> Other
- * Version:         2.1.2
+ * Version:         3.0.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -17,55 +17,99 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if ( ! class_exists( 'UM' ) ) return;
 
 
-add_action( 'um_submit_form_errors_hook__blockedemails', 'white_listed_email_domains', 20, 1 );
-add_filter( 'um_settings_structure', 'um_settings_structure_white_listed_email_domains', 10, 2 );
+Class UM_White_Listing_Email_Domains {
 
-function white_listed_email_domains( $args ) {
-    
-    if( !isset( $args['form_id'] )) return;
+    function __construct() {
 
-    $white_list_forms = UM()->options()->get( 'white_listed_email_domains_forms' );
-    if( !empty( $white_list_forms )) {
-        $white_list_forms = explode( ',', str_replace( ' ', '', $white_list_forms ));
-        if( !in_array( $args['form_id'], $white_list_forms )) return;
+        add_action( 'um_submit_form_errors_hook__blockedemails', array( $this, 'white_listed_email_domains' ), 20, 1 );
+        add_filter( 'um_settings_structure',                     array( $this, 'um_settings_structure_white_listed_email_domains' ), 10, 2 );
     }
 
-    $white_list = UM()->options()->get( 'white_listed_email_domains' );
-    if( empty( $white_list )) return;
-    $valid_email_domains = array_map( 'rtrim', explode( "\n", strtolower( $white_list )));
+    public function white_listed_email_domains( $args ) {
 
-    if( isset( $args['user_email'] ) && is_email( $args['user_email'] )) {
-        $email_domain = explode( '@', $args['user_email'] );        
-        if( !in_array( strtolower( $email_domain[1] ), $valid_email_domains ) &&
-            !in_array( strtolower( $args['user_email'] ), $valid_email_domains )) {
-            exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
+        if ( isset( $args['form_id'] ) && is_numeric( $args['form_id'] )) {
+
+            $white_list_forms = UM()->options()->get( 'white_listed_email_domains_forms' );
+            if ( ! empty( $white_list_forms ) && is_array( $white_list_forms )) {
+
+                $white_list_forms = array_map( 'sanitize_text_field', $white_list_forms );
+
+                if ( is_array( $white_list_forms ) && in_array( $args['form_id'], $white_list_forms )) {
+
+                    $white_list = UM()->options()->get( 'white_listed_email_domains' );
+                    if ( ! empty( $white_list )) {
+
+                        $valid_email_domains = array_map( 'rtrim', explode( "\n", strtolower( $white_list )));
+
+                        if ( isset( $args['user_email'] ) && is_email( $args['user_email'] )) {
+
+                            $email_domain = explode( '@', $args['user_email'] );
+
+                            if ( ! in_array( strtolower( $email_domain[1] ), $valid_email_domains ) &&
+                                ! in_array( strtolower( $args['user_email'] ), $valid_email_domains )) {
+                                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
+                            }
+                        }
+
+                        if ( isset( $args['username'] ) && is_email( $args['username'] )) {
+
+                            $email_domain = explode( '@', $args['username'] );
+
+                            if ( ! in_array( strtolower( $email_domain[1] ), $valid_email_domains ) &&
+                                ! in_array( strtolower( $args['username'] ), $valid_email_domains )) {
+                                exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    if( isset( $args['username'] ) && is_email( $args['username'] )) {
-        $email_domain = explode( '@', $args['username'] );        
-        if( !in_array( strtolower( $email_domain[1] ), $valid_email_domains ) &&
-            !in_array( strtolower( $args['username'] ), $valid_email_domains )) {
-            exit( wp_redirect( esc_url( add_query_arg( 'err', 'blocked_email' ))));
+    public function get_form_ids_register() {
+
+        $um_form_ids = array( '' );
+        $um_forms = get_posts( array( 'post_type' => 'um_form', 'numberposts' => -1, 'post_status' => array( 'publish' )));
+
+        if ( ! empty( $um_forms )) {
+            foreach ( $um_forms as $um_form ) {
+
+                $um_form_meta = get_post_meta( $um_form->ID );
+                if ( isset( $um_form_meta['_um_mode'][0] ) && $um_form_meta['_um_mode'][0] == 'register' ) {
+
+                    $um_form_ids[$um_form->ID] = $um_form->post_title;
+                }
+            }
         }
+
+        return $um_form_ids;
     }
+
+    public function um_settings_structure_white_listed_email_domains( $settings ) {
+
+        $settings['access']['sections']['other']['fields'][] = array(
+                    'id'      => 'white_listed_email_domains',
+                    'type'    => 'textarea',
+                    'label'   => __( 'White_Listing_Email_Domains - One entry per line', 'ultimate-member' ),
+                    'tooltip' => __( 'Enter one per line either domains name and/or user email addresses. This will block other e-mail addresses from being able to sign up to your site.', 'ultimate-member' ),
+                    'size'    => 'medium',
+                );
+
+        $settings['access']['sections']['other']['fields'][] = array(
+                    'id'      => 'white_listed_email_domains_forms',
+                    'type'    => 'select',
+                    'multi'   => true,
+                    'label'   => __( 'White_Listing_Email_Domains - Include these UM Forms in testing', 'ultimate-member' ),
+                    'options' => $this->get_form_ids_register(),
+                    'tooltip' => __( 'The UM Forms where these White listed e-mail addresses are tested.', 'ultimate-member' ),
+                );
+
+        return $settings;
+    }
+
 }
 
-function um_settings_structure_white_listed_email_domains( $settings ) {
-
-    $settings['access']['sections']['other']['fields'][] = array(
-        'id'      => 'white_listed_email_domains',
-        'type'    => 'textarea',
-        'label'   => __( 'White Listed Email Domains/Addresses (One entry per line)', 'ultimate-member' ),
-        'tooltip' => __( 'This will block other e-mail addresses from being able to sign up or sign in to your site.', 'ultimate-member' ),
-    );
-
-    $settings['access']['sections']['other']['fields'][] = array(
-        'id'      => 'white_listed_email_domains_forms',
-        'type'    => 'text',
-        'label'   => __( 'White Listed Email Forms (Enter form_id comma separated)', 'ultimate-member' ),
-        'tooltip' => __( 'The UM Form IDs where these White listed e-mail addresses are tested.', 'ultimate-member' ),
-    );
+new UM_White_Listing_Email_Domains();
 
     return $settings;
 }
